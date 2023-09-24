716cb057afa88a39309dcfe3996ab3b61b571735
Raju Shrestha <shrestharaju010101@gmail.com>
2023-09-24 22:50:39 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("hello world")
+// const a=10;
+// const b=5;
+// console.log(a)
+// console.log(b)
+// const sum= a+b;
+// console.log(sum)
+
+// Array
+const numbers=[10,20,30]
+// console.log(numbers [1])
+
+// to calculate length of array
+console.length(numbers.length)
